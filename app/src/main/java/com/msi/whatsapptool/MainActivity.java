c77609025a0b748c2522a88813167d48a3563b1b
JHONNYCODER <realanujrajput@gmail.com>
2022-10-02 20:35:13 +0530

    Full App Working .. Dark Mode is optimised and Icons are also Added as well.

--- app/src/main/java/com/msi/whatsapptool/MainActivity.java
+++ app/src/main/java/com/msi/whatsapptool/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
